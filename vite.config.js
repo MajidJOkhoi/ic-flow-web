4857ff70689876f953796b8ba3d4ad57235aea45
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-08 10:11:35 +0500

    refresh problem solved13

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     proxy: {
       "/api":"https://flow-backend-ic-production.up.railway.app"
