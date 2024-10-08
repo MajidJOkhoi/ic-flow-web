5463dbe12d457dde749fe9a99ff1a1e50e0359f3
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-08 09:51:51 +0500

    refresh problem solved1

--- src/pages/teamlead/teamattendance/AbsentTeamMember.jsx
+++ src/pages/teamlead/teamattendance/AbsentTeamMember.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import api from '../../../api'
-
 import {
   Table,
   TableBody,
@@ -16,6 +14,7 @@ import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Breadcrumb, BreadcrumbItem , BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Link } from "react-router-dom";
+import api from '../../../api'
 
 
 const AbsentTeamMember = () => {
